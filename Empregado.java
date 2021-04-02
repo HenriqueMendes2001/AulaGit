0f031f5d2025120d687694d2a5ee1dfb0d3180ef
Henrique <iquinhosilva@hotmail.com>
2021-04-02 17:57:20 -0300

    Cálculo de salario realizado com o tipo

---
+++ Empregado.java
@@ -0,0 +1,32 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    
+    public double calcularSalario(){
+        switch(tipo){
+            case 1:
+                return salario;
+            case 2:
+                return salario + salario * comissao;
+            default:
+                return 0;
+        }
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public void setIdade (int idade){
+        this.idade = idade;
+    }
+}
